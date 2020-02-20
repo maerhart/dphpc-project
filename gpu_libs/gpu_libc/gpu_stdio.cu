b16e0f12d9b565f4f2e5cc32ce7ddda29be27908
Timo Schneider <timos@inf.ethz.ch>
2020-02-20 15:14:35 +0100

    start implementing delegate

--- gpu_libs/gpu_libc/gpu_stdio.cu
+++ gpu_libs/gpu_libc/gpu_stdio.cu
@@ -22,8 +22,29 @@ __device__ int fflush(FILE */*__stream*/) {
 }
 
 __device__ FILE *fopen(const char *pathname, const char *mode) {
-    NOT_IMPLEMENTED
-    return stdout;
+    fopen_args_t* args = allocate_host_mem(sizeof(fopen_args_t));
+    args->calltype = FOPEN;
+    if (strlen(pathname) > sizeof(args.pathname))
+        errno = ENOSPACE;
+    	free_host_mem(args);
+        return NULL;
+    strcpy(args->pathname, pathname);
+    if (strlen(mode) > sizeof(args->mode)) {
+    	errno = ENOSPACE;
+	free(args);
+	return NULL;
+    }
+    strcpy(args->mode, mode);
+    delegate_to_host(args, sizeof(fopen_args_t), BLOCKING);
+    if (args->retval == NULL) {
+      errno = args->errno;
+      free_host_mem(args);
+      return NULL;
+    }
+    FILE* res = allocate_dev_mem(sizeof(FILE));
+    memcpy(res, args->file, sizeof(FILE));
+    free_host_mem(args);
+    return res;    
 }
 
 __device__ int fclose(FILE *stream) {
