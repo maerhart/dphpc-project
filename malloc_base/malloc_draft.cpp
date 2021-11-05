12c8a3370327ba941ecfdb9a0101fdf23a110fef
Florim Memedi <flo.memedi@gmail.com>
2021-11-05 10:45:33 +0100

    free, memory range check

--- malloc_base/malloc_draft.cpp
+++ malloc_base/malloc_draft.cpp
@@ -27,28 +27,41 @@ int get_free_list_index(int64_t size) {
 }
 
 // size in bytes
-void* custom_malloc(size_t size, free_header** free_blocks)
+void* custom_malloc(size_t size, free_header** free_blocks, const void* range_start, const void* range_end)
 {
 	// TODO
 	return NULL;
 }
 
-void custom_free(void* ptr, free_header** free_list)
+/*
+* @param ptr: block to be freed.
+* The parameters below are constant for the whole execution of the allocator.
+* @param free_list: points to free blocks datastructure
+* @param range_start, range_end: valid range for our memory allocation
+*/
+void custom_free(void* ptr, free_header** free_list, const void* range_start, const void* range_end)
 {
+	// check for valid memory range
+	if (!(ptr >= range_start && ptr <= range_end)) {
+		return;
+	}
+
 	// read the header before data starts
 	full_header* header = (full_header*)ptr - 1; // -1: points to start of header
 
 	// check if block already free, do nothing
 	if (is_free(header)) return;
 
-	// check if next block is free
+	// check if next block is free. check if we are still in valid memory range.
 	full_header* next_header = (full_header*)((char*)header + sizeof(header) + header->size); // working in bytes
-	// TODO: check if we are still in valid memory range
-	while (is_free(next_header)) {
+	while (is_free(next_header)
+		&& next_header >= (full_header*)range_start
+		&& next_header <= (full_header*)range_end)
+	{
 		// free blocks store header as data
 		next_header = (full_header*)((char*)next_header + next_header->size); // working in bytes
 	}
-	
+
 	// combine all consecutive free blocks and add to free_list
 	free_header* new_header = (free_header*)header;
 	new_header->size = (char*)next_header - (char*)header; // gives a positive size => free
@@ -75,19 +88,19 @@ void test()
 
 		h.size = -1;
 		assert(is_free(&h));
-		
+
 		h.size = 0x8000000000000000;
 		//std::cout << h.size << std::endl;
 		assert(is_free(&h));
 	}
-	
+
 	// get_free_list_index
 	{
 		assert(get_free_list_index(1) == 0);
 		assert(get_free_list_index(2) == 1);
 		assert(get_free_list_index(100) == 7); // 2^6 = 64
 	}
-	
+
 }
 
 int main(int argc, char* argv[])
@@ -95,16 +108,17 @@ int main(int argc, char* argv[])
 	// run some simple unit tests, only in debug mode!
 	test();
 
+	int mem_size = 16;
 
-	free_header* initial_block = (free_header*)malloc(16);
-	initial_block->size = 16;
+	free_header* initial_block = (free_header*)malloc(mem_size);
+	initial_block->size = mem_size;
 	initial_block->next = NULL;
 
 	// up to 16 bytes sized blocks for now
 	free_header* free_blocks[5];
 	free_blocks[4] = initial_block;
 
-	int* test_int = (int*)custom_malloc(sizeof(int), free_blocks);
+	int* test_int = (int*)custom_malloc(sizeof(int), free_blocks, (void*)initial_block, (char*)initial_block + mem_size);
 	*test_int = 5;
 
 
