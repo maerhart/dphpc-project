f32ee854944651ac8d7c2883bb982f5f178baf22
Anton Schäfer <scanton@ethz.ch>
2021-11-06 16:17:07 +0100

    Working version of malloc and free

--- malloc_base/malloc_draft.cpp
+++ malloc_base/malloc_draft.cpp
@@ -1,12 +1,14 @@
 ﻿#include <math.h>
-#include <iostream>
+#include <iostream> // TODO remove when moving tests to separate file
 #include <assert.h>
 #include <tuple>
 
-const int LOG_MIN_BLOCK_SIZE = 3; // allocate at least eight bytes
-const size_t MIN_BLOCK_SIZE = pow(2.0, LOG_MIN_BLOCK_SIZE);
+const int LOG_MIN_BLOCK_SIZE = 4; // allocate at least 16 bytes
+const size_t MIN_BLOCK_SIZE = pow(2.0, LOG_MIN_BLOCK_SIZE); // has to be of at least size sizeof(free_header) - sizeof(full_header)
 const size_t MAX_BLOCK_SIZE = pow(2.0, 32.0); // has to be power of 2 -- allocate at most 4 GB
 const int LEN_FREE_LIST = ((int) log2(MAX_BLOCK_SIZE)) - ((int) log2(MIN_BLOCK_SIZE)) + 1;
+
+const int64_t SIZE_MASK = ~(((int64_t) 1) << 63);
 
 /* free list design:
  *
@@ -16,24 +18,103 @@ const int LEN_FREE_LIST = ((int) log2(MAX_BLOCK_SIZE)) - ((int) log2(MIN_BLOCK_S
  *
  */
 
+// TODO alignment requirements?
+
 
 // header of a free block
 struct free_header {
-	int64_t size; // size of the payload (excluding header size)
+	int64_t size; // size of the payload (excluding header size of full_header)
 	free_header* next;
+	free_header** prev_next_ptr;
 };
 
 struct full_header {
 	// negative means free
 	int64_t size; // size of the payload (excluding header size)
-	// pointer to previous block
-	full_header* prev;
+
+	/* planned to potentially introduce for better freeing:
+	 * full_header* prev;
+	 */
 };
 
-// sign-bit indicates if block is free
-bool is_free(full_header* header) {
-	return header->size < 0;
+
+/*
+ * determine whether pointer points to free header or full header
+ *
+ * @param header Pointer to legal header
+ * @return True iff points to free header
+ */
+bool is_free(void* header) {
+	assert(((full_header*) header)->size == (*((int64_t*)header))); // TODO remove
+	return ((*((int64_t*) header)) & ~SIZE_MASK) != 0; // first bit of size field 1 means block is free
 }
+
+/*
+ * determine whether pointer points to free header or full header
+ *
+ * @param header Pointer to legal header
+ * @return payload size of block header points to
+ * 	(total block size including header - sizeof(full_header))
+ */
+size_t get_size(void* header) {
+	assert(((full_header*) header)->size == (*((int64_t*)header))); // TODO remove
+	return (*((int64_t*) header)) & SIZE_MASK;
+}
+
+/**
+ * Set block size
+ *
+ * @param header The header of the block
+ * @param size The payload size of the block
+ * 		requires size < 2^63
+ */
+void set_size(full_header* header, size_t size) {
+	header->size = ((int64_t) size) | (header->size & ~SIZE_MASK);
+}
+
+/**
+ * Set block size
+ *
+ * @param header The header of the block
+ * @param size The payload size of the block
+ * 		requires size < 2^63
+ */
+void set_size(free_header* header, size_t size) {
+	header->size = ((int64_t) size) | (header->size & ~SIZE_MASK);
+}
+
+/**
+ * Make a free block
+ *
+ * @param ptr Pointer to memory location where to set up free block
+ * @param size Payload size of the free block
+ * @return Pointer to the free block
+ */
+free_header* make_free_block(void* ptr, size_t size) {
+	free_header* header = (free_header*) ptr;
+	// set free bit to 1
+	header->size = ((int64_t) 1) << 63;
+	set_size(header, size);
+	header->next = NULL;
+	header->prev_next_ptr = NULL;
+	return header;
+}
+
+/**
+ * Make a full/allocated block
+ *
+ * @param ptr Pointer to memory location where to set up block
+ * @param size Payload size of the block
+ * @return Pointer to the block
+ */
+full_header* make_full_block(void* ptr, size_t size) {
+	full_header* header = (full_header*) ptr;
+	// set free bit to 0
+	header->size = 0;
+	set_size(header, size);
+	return header;
+}
+
 
 /**
  * Retrieve index into free list for extracting a block of at least the given size
@@ -61,12 +142,70 @@ int get_free_list_insertion_index(int64_t size) {
   	if (size < MIN_BLOCK_SIZE) {
 		assert(false); // TODO how to throw here?
 	}
-	return floor(log2(size)) - LOG_MIN_BLOCK_SIZE;
+	int insertion_index = ((int) log2(size)) - LOG_MIN_BLOCK_SIZE;
+	return std::min(insertion_index, LEN_FREE_LIST - 1);
+}
+
+/**
+ * Insert a free block into the free list of the appropriate size
+ *
+ * @param free_list The array of free lists
+ * @param header Pointer to a valid free block
+ */
+void insert_in_free_list(free_header** free_list, free_header* header) {
+	assert(is_free(header));
+	int index = get_free_list_insertion_index(get_size(header));
+	header->next = free_list[index];
+	free_list[index] = header;
+
+	// set prev_next_ptr  for header and block after
+	header->prev_next_ptr = &free_list[index];
+	if (header->next != NULL) {
+		header->next->prev_next_ptr = &(header->next);
+	}
 }
 
 
 /**
+ * Remove a free block from the free list
+ *
+ * @param header Pointer to a valid free block that is contained in a free list
+ */
+void remove_from_free_list(free_header* header) {
+	if (header->prev_next_ptr != NULL) {
+		*(header->prev_next_ptr) = header->next;
+	}
+	if (header->next != NULL) {
+		header->next->prev_next_ptr = header->prev_next_ptr;
+	}
+}
+
+/**
+ * Pop a block from a free list and make it a full block
+ *
+ * @param free_list The array of free lists
+ * @param index Index specifying from which free list (/block size) to pop
+ * 		requires that free_list at index is non empty
+ *
+ * @return The first free block from the specified list as a usable full block
+ */
+full_header* pop_free_list(free_header** free_list, int index) {
+	assert(free_list[index] != NULL);
+	assert(index >= 0 && index < LEN_FREE_LIST);
+
+	free_header* block_free = free_list[index];
+
+	remove_from_free_list(block_free);
+
+	return make_full_block(block_free, get_size(block_free));
+}
+
+
+
+/**
  * Initialize the free list
+ *
+ * To clean up just free free list
  *
  * @param mem_size Number of bytes to be used for first blocks and free list
  * @return pointer to free list
@@ -82,23 +221,20 @@ std::tuple<free_header**, void*, void*> init_free_list(size_t mem_size) {
 	}
 
   	// create empty free lists
-	free_header** free_list = (free_header**)malloc(size_free_list);  // TODO malloc only once?
+	// malloc entire memory chunk used for free list and malloc at once
+	free_header** free_list = (free_header**)malloc(mem_size);
 	for (int i = 0; i < LEN_FREE_LIST; i++) {
 	  free_list[i] = NULL;
 	}
 
 	size_t available_mem = mem_size - size_free_list;
   
-  	// allocate memory
-	free_header* initial_block = (free_header*)malloc(available_mem);
-
-	// build initial block
+  	// give rest of memory to initial block
+	char* ptr_initial_block = ((char*)free_list) + size_free_list;
 	size_t payload_size = available_mem - sizeof(full_header);
-	initial_block->size = payload_size;
-	initial_block->next = NULL;
-
+	free_header* initial_block = make_free_block(ptr_initial_block, payload_size);
 	// insert block into free list
-	free_list[get_free_list_insertion_index(payload_size)] = initial_block;
+	insert_in_free_list(free_list, initial_block);
 
 	// compute range of usable memory
 	void* range_start = initial_block;
@@ -118,14 +254,42 @@ void* custom_malloc(size_t size, free_header** free_list, const void* range_star
 	  assert(false); // TODO how to throw here? or return NULL?
 	}
 
+	// round up size to min block size
+	size = MIN_BLOCK_SIZE * ceil((1.0 * size)/MIN_BLOCK_SIZE);
+
 	int free_list_index = get_free_list_extraction_index(size);
 
 
-  	// TODO check size legal?
+	// find list with free block
+	while (free_list[free_list_index] == NULL) {
+		free_list_index++;
+		if (free_list_index == LEN_FREE_LIST) {
+	  		assert("out of memory" && false); // TODO how to throw here? or return NULL?
+		}
+	}
+
+	// retrieve block
+	full_header* block = pop_free_list(free_list, free_list_index);
+	size_t available_payload_size = get_size(block);
+
+
+	// split block if necessary
+	if (available_payload_size >= sizeof(full_header) + MIN_BLOCK_SIZE + size) {
+		// split block
+		
+		// reduce size of old block
+		set_size(block, size);
+
+		// create new block
+		free_header* new_block = make_free_block(
+			((char*) block) + sizeof(full_header) + size, 		// ptr to start of free area
+			available_payload_size - size - sizeof(full_header)	// size: need to accomodate new_blocks header and payload of allocated block
+		);
+
+		insert_in_free_list(free_list, new_block);
+	}
 	
-  
-	// TODO
-	return NULL;
+	return ((char*)block) + sizeof(full_header);
 }
 
 /*
@@ -142,29 +306,32 @@ void custom_free(void* ptr, free_header** free_list, const void* range_start, co
 	}
 
 	// read the header before data starts
-	full_header* header = (full_header*)ptr - 1; // -1: points to start of header
+	full_header* header = ((full_header*)ptr) - 1; // -1: points to start of header
 
-	// check if block already free, do nothing
-	if (is_free(header)) return;
+	// check if block already free
+	if (is_free(header)) {
+		assert(false); // freeing twice is undefined behaviour  TODO how to throw here
+	}
 
 	// check if next block is free. check if we are still in valid memory range.
-	full_header* next_header = (full_header*)((char*)header + sizeof(header) + header->size); // working in bytes
+	char* next_header = ((char*)header) + sizeof(full_header) + get_size(header); // working in bytes
 	while (is_free(next_header)
-		&& next_header >= (full_header*)range_start
-		&& next_header <= (full_header*)range_end)
+		&& next_header >= (char*)range_start
+		&& next_header <= (char*)range_end)
 	{
-		// free blocks store header as data
-		next_header = (full_header*)((char*)next_header + next_header->size); // working in bytes
+		// the following block is free so we want to merge it with our freed block
+		// -> remove from its free list as it will be part of the new bigger free block
+		
+		// remove from free list
+		remove_from_free_list((free_header*) next_header);
+
+		// size always specifies payload size that block can fit if full
+		next_header = next_header + sizeof(full_header) + get_size(next_header);
 	}
 
 	// combine all consecutive free blocks and add to free_list
-	free_header* new_header = (free_header*)header;
-	new_header->size = (char*)next_header - (char*)header; // gives a positive size => free
-	int index = get_free_list_insertion_index(new_header->size);
-	new_header->next = free_list[index]; // 
-	free_list[index] = new_header;
-
-	return;
+	free_header* freed_block = make_free_block(header, next_header - ((char*)header) - sizeof(full_header));
+	insert_in_free_list(free_list, freed_block);
 }
 
 // do some simple tests
@@ -174,7 +341,7 @@ void test()
 	{
 		full_header h;
 		h.size = 0;
-		h.prev = NULL;
+		// h.prev = NULL;
 
 		assert(!is_free(&h));
 
@@ -185,7 +352,6 @@ void test()
 		assert(is_free(&h));
 
 		h.size = 0x8000000000000000;
-		//std::cout << h.size << std::endl;
 		assert(is_free(&h));
 	}
 
@@ -205,32 +371,29 @@ void test()
 		assert(get_free_list_extraction_index(MAX_BLOCK_SIZE - 1) == LEN_FREE_LIST - 1);
 	}
 
-	/*
 	// repeatedly allocate individual ints and sum them up
 	{
-		#define MEM_SIZE 1024
-		#define BLOCK_IND 10
+		const int MEM_SIZE = 8192;
+		const int num_ints = 100; // need enough space for num_ints ints and free list and headers
 
-		free_header* initial_block = (free_header*)calloc(MEM_SIZE, 1);
-		initial_block->size = MEM_SIZE;
-		initial_block->next = NULL;
-
-		// put block in free list
-		free_header* free_blocks[BLOCK_IND + 1];
-		free_blocks[BLOCK_IND] = initial_block;
+		// init free list
+		std::tuple<free_header**, void*, void*> init_data = init_free_list(10000); // contains free list, range_start, range_end
+		free_header** free_list = std::get<0>(init_data);
+		void* range_start = std::get<1>(init_data);
+		void* range_end = std::get<2>(init_data);
 
 		// repeat test multiple times on the same free_blocks datastructure
 		for (int num_runs = 0; num_runs < 10; ++num_runs) {
 
 			// array with results
-			const int num_ints = MEM_SIZE / sizeof(int);
 			int* result[num_ints];
 			int* reference[num_ints];
+
 
 			// fill available space with ints 0, 1, ...
 			for (int i = 0; i < num_ints; ++i) {
 				// custom malloc
-				int* val = (int*)custom_malloc(sizeof(int), free_blocks, (void*)initial_block, (char*)initial_block + MEM_SIZE);
+				int* val = (int*)custom_malloc(sizeof(int), free_list, range_start, range_end);
 				assert(val); // No null pointers
 				*val = i;
 				result[i] = val;
@@ -259,20 +422,16 @@ void test()
 			assert(ref_sum == (num_ints - 1) * (num_ints) / 2); // analytic solution
 			assert(res_sum == ref_sum); // reference solution
 
-
 			// free for the next round
 			for (int i = 0; i < num_ints; ++i) {
-				custom_free(result[i], free_blocks, (void*)initial_block, (char*)initial_block + MEM_SIZE);
+				custom_free(result[i], free_list, range_start, range_end);
 				free(reference[i]);
 			}
 
 		}
-
-
 		// clean up
-		free(initial_block);
+		free(free_list);
 	}
-	*/
 
 }
 
@@ -281,11 +440,18 @@ int main(int argc, char* argv[])
 	// run some simple unit tests, only in debug mode!
 	test();
 
+	std::cout << "Tests passed" << std::endl;
+
 	std::tuple<free_header**, void*, void*> init_data = init_free_list(10000); // contains free list, range_start, range_end
 
 
 	int* test_int = (int*)custom_malloc(sizeof(int), std::get<0>(init_data), std::get<1>(init_data), std::get<2>(init_data));
-	//*test_int = 5;
+	*test_int = 5;
+
+	std::cout << "Malloc done" << std::endl;
+
+	// clean up by freeing free list => frees all memory given to custom malloc
+	free(std::get<0>(init_data));
 
 	return 0;
 }
