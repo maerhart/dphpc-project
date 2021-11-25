d09358d8c454904b5a09519f9cf81533c571bea4
Florim Memedi <flo.memedi@gmail.com>
2021-11-25 17:15:10 +0100

    v1: simple superblock with strided headers, passing simple tests

--- dynamic_allocator/dynamic_allocator.cu
+++ dynamic_allocator/dynamic_allocator.cu
@@ -24,125 +24,70 @@ struct s_header {
 };
 
 
-
-__device__ int num_avail_blocks = 0;
+// memory layout of a superblock
+// s_header, blocksize x [pointer to s_header, data]
 __device__ void* malloc_v1(size_t size) {
-	
 	__shared__ void* superblock;
 
-	if (num_avail_blocks < 1) {
+	if (threadIdx.x == 0) {
 		// allocate new superblock
-		int n = 32;
-		superblock = malloc(sizeof(s_header) + n * (sizeof(s_header*) + size));
+		superblock = malloc(sizeof(s_header) + blockDim.x * (sizeof(s_header*) + size));
+		if (!superblock) return NULL;
 		
+		// initialize header	
 		struct s_header* header;
 		header = (s_header*)superblock;
-		//s_header *header = (s_header*)superblock;
 		header->counter = blockDim.x;
-		
-		//s_header** ref;
-		//ref = (s_header**)((s_header*)superblock + 1);
-		//*ref = header;
-		
-
-			
-		//*((s_header*)superblock + 1) = *header;
-		//void* ptr = (char*)superblock + sizeof(s_header) + sizeof(s_header*);
-		
-		//return ptr; 
 	}
 	__syncthreads();
-	//auto block = cooperative_groups::this_thread_block();
-	//block.sync();
 
-
-
-    void* ptr = (char*)superblock + sizeof(s_header) + threadIdx.x * (size + sizeof(s_header*)) + sizeof(s_header*);
-	s_header** header = (s_header**)((char*)ptr - sizeof(s_header*));
-	*header = (s_header*)superblock;
-
-	return ptr;
+	// ptr to individual memory offset
+    s_header* ptr = (s_header*)((char*)superblock + sizeof(s_header) + threadIdx.x * (size + sizeof(s_header*)));
+	// set pointer to superblock header
+	*ptr = *(s_header*)superblock;
+	// return the pointer to the data section
+	return (void*)(ptr + 1);
 }
 
 __device__ void free_v1(void* memptr) {
-	s_header* header = ((s_header*)memptr - 1);    
-	int count = atomicAdd(&(header->counter), -1);
-
+	// decrease counter
+	s_header* header = (s_header*)memptr - 1;    
+	int count = atomicSub(&(header->counter), 1);
+	
+	// last thread frees superblock
 	if (count == 0) free(header);
 }
 
 
 
-// repeatedly allocate individual ints and sum them up
-__global__ void test(int *resulting_sums) {
+// allocate one int per thread and set to threadId
+__global__ void test(int *resulting_ids) {
 	
 	int id = (blockIdx.x*blockDim.x + threadIdx.x);
-
 	int* val = (int*)malloc_v1(sizeof(int));
 	*val = id;
-	//printf("test");
-	resulting_sums[id] = *val;
-	free_v1(val);
-
-	/*
-	// array with results
-	const int num_ints = 100;
-	int* result[num_ints];
-
-	// fill available space with ints 0, 1, ...
-	for (int i = 0; i < num_ints; ++i) {
-		// custom malloc
-		int* val = (int*)malloc_baseline(sizeof(int));
-		assert(val); // No null pointers
-		*val = i;
-		result[i] = val;
-
-	}
-
-	// compare results
-	int res_sum = 0;
-	for (int i = 0; i < num_ints; ++i) {
-
-		res_sum += *(result[i]);
-
-		// correct value stored
-		assert(*(result[i]) == i);
-	}
-
-	// correct sum
-	assert(res_sum == (num_ints - 1) * (num_ints) / 2); // analytic solution
-	int id = (blockIdx.x*blockDim.x + threadIdx.x);
-	resulting_sums[id] = res_sum;
-
-	// free for the next round
-	for (int i = 0; i < num_ints; ++i) {
-		free_baseline(result[i]);
-	}
-	*/
-
+	resulting_ids[id] = *val;
 }
 
 int main(int argc, char* argv[]) {
 	// run some simple unit tests, only in debug mode!
-	int blocks = 1;
+	int blocks = 100;
 	int threads_per_block = 32;
 	int total_threads = blocks * threads_per_block;
 
-	int resulting_sums[total_threads];
-	int *d_resulting_sums;
-	cudaMalloc(&d_resulting_sums, total_threads*sizeof(int));	
+	int resulting_ids[total_threads];
+	int *d_resulting_ids;
+	cudaMalloc(&d_resulting_ids, total_threads*sizeof(int));	
 
-	test<<<blocks, threads_per_block>>>(d_resulting_sums);
+	test<<<blocks, threads_per_block>>>(d_resulting_ids);
 	cudaDeviceSynchronize(); // to allow for printf in kernel code
-	cudaMemcpy(resulting_sums, d_resulting_sums, total_threads*sizeof(int), cudaMemcpyDeviceToHost);
+	cudaMemcpy(resulting_ids, d_resulting_ids, total_threads*sizeof(int), cudaMemcpyDeviceToHost);
 
+	// sum up all ids, should match to the sum from 0 to total_threads
 	bool passed = true;
 	int sum = 0;
 	for (int i = 0; i < total_threads; ++i) {
-		sum += resulting_sums[i];
-		if (resulting_sums[i] != 5) {
-			//passed = false;
-		}
+		sum += resulting_ids[i];
 	}
 	// sum up 0 to total_threads
 	passed = sum == (total_threads - 1) * (total_threads) / 2;
