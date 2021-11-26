2ce26836fa2f4f2062af9e38c38ab7c8ba9f6d6e
Anton Schäfer <scanton@ethz.ch>
2021-11-26 16:29:10 +0100

    Benchmark warp malloc

--- strided_benchmarks/strided_write.cu
+++ strided_benchmarks/strided_write.cu
@@ -33,6 +33,66 @@ __global__ void strided_write(int empty_padding, float* arr, int num_floats, clo
 }
 
 /**
+ * Allocate one array for each warp and write floats into it, leaving unused space between different threads' array segments
+ *
+ * @param empty_padding How many 4-byte segments to leave empty at end of thread segment
+ * @param arr Pointer to allocated array
+ * @param num_floats Number of floats to write
+ * @param runtime Array to hold the runtime of each thread
+ */
+__global__ void strided_write_warp_malloc(int empty_padding, int num_floats, clock_t* runtime) {
+
+	// for each block build array that holds shared warp arrays for each warp
+	__shared__ float** warp_arrs;
+	if (threadIdx.x == 0) {
+		warp_arrs = (float**) malloc(sizeof(float*) * ((blockDim.x / 32) + 1));
+		if (!warp_arrs) {
+			printf("Failed to malloc shared warp array pointers");
+		}
+	}
+	__syncthreads();
+
+	// allocate shared array for entire warp
+	int lane_id = threadIdx.x % 32;
+	int warp_id = threadIdx.x / 32;
+	if (lane_id == 0) {
+		float* warp_arr = (float*) malloc(32 * sizeof(float) * (empty_padding + num_floats));
+		if (!warp_arr) {
+			printf("Failed to malloc %d floats\n", 32 * (empty_padding + num_floats));
+		}
+		assert(warp_arrs != NULL);
+		warp_arrs[warp_id] = warp_arr;
+	}
+	__syncthreads();
+	assert(warp_arrs != NULL);
+	assert(warp_arrs[warp_id] != NULL);
+
+	int id = (blockIdx.x*blockDim.x + threadIdx.x);
+	float* ptr = warp_arrs[warp_id] + lane_id * (empty_padding + num_floats);
+
+	clock_t start_time = clock();
+
+	for (int i = 0; i < num_floats; i++) {
+		ptr[i] = id + i;
+	}
+
+	clock_t end_time = clock();
+	runtime[id] = end_time - start_time;
+	if (end_time < start_time) {
+		printf("Clock overflow\n");
+		assert(false);
+	}
+
+	__syncthreads();
+	if (lane_id == 0) {
+		free(warp_arrs[warp_id]);
+	}
+	if (threadIdx.x == 0) {
+		free(warp_arrs);
+	}
+}
+
+/**
  * Allocate one array for each block and write floats into it, leaving unused space between different threads' array segments
  *
  * @param empty_padding How many 4-byte segments to leave empty at end of thread segment
@@ -74,6 +134,7 @@ __global__ void strided_write_block_malloc(int empty_padding, int num_floats, cl
 		free(block_arr);
 	}
 }
+
 
 /**
  * Malloc an array and Write floats into it
@@ -147,6 +208,15 @@ int main(int argc, char **argv) {
 	run_benchmark(num_runs, num_warmup, mean_runtimes, max_runtimes, blocks, threads_per_block,
 			[empty_padding, num_floats](clock_t* runtimes, int b, int t) -> void {
 				run_strided_write(runtimes, b, t, empty_padding, num_floats);
+			}
+		     );
+	print_arr(mean_runtimes, num_runs);
+	//print_arr(max_runtimes, num_runs);
+
+	// run warp malloc write
+	run_benchmark(num_runs, num_warmup, mean_runtimes, max_runtimes, blocks, threads_per_block,
+			[empty_padding, num_floats](clock_t* runtimes, int b, int t) -> void {
+				strided_write_warp_malloc<<<b, t>>>(empty_padding, num_floats, runtimes);
 			}
 		     );
 	print_arr(mean_runtimes, num_runs);
