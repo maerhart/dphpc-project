8a2ade31a23f26e1cf32af0a3b5851bbe682ddc6
Nils Blach <nils.blach@gmx.de>
2021-11-27 11:48:08 +0100

    Dynamic malloc using HashMap

--- gpu_libs/gpu_malloc/dyn_malloc_no_header.cu
+++ gpu_libs/gpu_malloc/dyn_malloc_no_header.cu
@@ -1,25 +1,31 @@
-
-struct KeyValue
-{
-    uint32_t key;
-    uint32_t value;
+struct KeyValue {
+    void *key;
+    void *value;
 }
 
 const uint32_t capacity = 1024 * 1024; // 8 MB per block
 
 const uint32_t max_chain = capacity / 2;
 
-const uint32_t empty = 0xffffffff;
+const void *empty = (0xffffffff << 32) | 0xffffffff;
 
 // 32 bit Murmur3 hash
-__device__ uint32_t hash(uint32_t k)
-{
-    k ^= k >> 16;
-    k *= 0x85ebca6b;
-    k ^= k >> 13;
-    k *= 0xc2b2ae35;
-    k ^= k >> 16;
-    return k & (capacity-1);
+//__device__ uint32_t hash(uint32_t k) {
+//    k ^= k >> 16;
+//    k *= 0x85ebca6b;
+//    k ^= k >> 13;
+//    k *= 0xc2b2ae35;
+//    k ^= k >> 16;
+//    return k & (capacity-1);
+//}
+
+__device__ uint64_t hash(uint64_t x) {
+    x ^= x >> 27;
+    x *= 0x3C79AC492BA7B653UL;
+    x ^= x >> 33;
+    x *= 0x1C69B3F74AC4AE35UL;
+    x ^= x >> 27;
+    return x & (capacity-1);
 }
 
 __device__ void create_hashtable(KeyValue** table) {
@@ -29,12 +35,12 @@ __device__ void create_hashtable(KeyValue** table) {
 }
 
 __device__ void insert(KeyValue* hashtable, KeyValue* kv, bool *success) {
-    uint32_t key = kv->key;
-    uint32_t value = kv->value;
-    uint32_t slot = hash(key);
-    uint32_t slot_start = slot;
+    void *key = kv->key;
+    void *value = kv->value;
+    uint64_t slot = hash((uint64_t) key);
+    uint64_t slot_start = slot;
     while (true) {
-        uint32_t prev = atomicCAS(&hashtable[slot].key, empty, key);
+        void *prev = atomicCAS(&hashtable[slot].key, empty, key);
         if (prev == empty || prev == key) {
             hashtable[slot].value = value;
             *success = true;
@@ -49,8 +55,8 @@ __device__ void insert(KeyValue* hashtable, KeyValue* kv, bool *success) {
 }
 
 __device__ void lookup(KeyValue *hashtable, KeyValue *kv) {
-    uint32_t key = kv->key;
-    uint32_t slot = hash(key);
+    void *key = kv->key;
+    uint64_t slot = hash((uint64_t)key);
 
     while (true) {
         if(hashtable[slot].key == key) {
@@ -66,8 +72,8 @@ __device__ void lookup(KeyValue *hashtable, KeyValue *kv) {
 
 // Cannot remove the assigned slot, otherwise all keys that come after ''will get lost''
 __device__ void remove(KeyValue *hashtable, KeyValue *kv) {
-    uint32_t key = kv->key;
-    uint32_t slot = hash(key);
+    void *key = kv->key;
+    uint64_t slot = hash((uint64_t)key);
 
     while (true) {
         if(hashtable[slot].key == key) {
@@ -85,40 +91,70 @@ __device__ void destroy_hashtable(KeyValue **hashtable) {
     *hashtable = NULL;
 }
 
+__shared__ KeyValue *table;
+__shared__ static void **mem;
+
 __device__ void *dyn_malloc(size_t size, bool coalesced) {
+    //init missing
     if(!coalesced) {
         // Ensure compatibility with coalesced case
         // | counter 4B | returned ptr... |
-        void *ptr = malloc(size + sizeof(int));
-        int *counter = (int*) ptr;
+        void *ptr = malloc(size + sizeof(uint32_t));
+        uint32_t *counter = (uint32_t*) ptr;
         *counter = 1;
-        return counter+1;
+        KeyValue kv = {
+            .key = (void *)(counter+1);
+            .value = (void *)(counter+1);
+        };
+        bool success;
+        insert(table, &kv, &success);
+        if(success) {
+            return counter+1;
+        } else {
+            return NULL;
+        }
     }
 
     if (threadIdx.x == 0) {
-        *mem = malloc((size + sizeof(int*)) * blockDim.x + sizeof(int));
+        *mem = malloc(size * blockDim.x + sizeof(uint32_t));
         // Initialize counter
-        **(int**)mem = blockDim.x;
+        **(uint32_t**)mem = blockDim.x;
     }
 
     auto block = cooperative_groups::this_thread_block();
 
     block.sync();
 
-    void *ptr = (char*)*mem + sizeof(int) + sizeof(int*) + threadIdx.x * (size + sizeof(int*));
-    int **counter_ptr = (int**) ((char*)ptr-sizeof(int*));
-    *counter_ptr = (int*)*mem;
+    void *ptr = (char*)*mem + sizeof(uint32_t) + threadIdx.x * size;
+    KeyValue kv = {
+        .key = (void *) ptr;
+        .value = (void *) *mem;
+    };
+
+    bool success;
+    insert(table, &kv, &success);
 
     block.sync();
 
-    return ptr;
+    if(success) {
+        return ptr;
+    } else {
+        return NULL;
+    }
 }
 
 __device__ void dyn_free(void *memptr) {
-    int *counter_ptr = *((int**)memptr-1);
-    int counter = atomicAdd(counter_ptr, -1);
+    KeyValue kv = {
+        .key = memptr;
+        .value = empty;
+    }
 
-    if (counter==0) {
+    lookup(table, &kv);
+    uint32_t *counter_ptr = (uint32_t *) kv.value;
+    uint32_t counter = atomicAdd(counter_ptr, -1);
+    remove(table, &kv);
+
+    if (counter == 1) {
         free(counter_ptr);
     }
 }
