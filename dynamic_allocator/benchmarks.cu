219556556cb86f828389cfe47ec70334f45f605e
Florim Memedi <flo.memedi@gmail.com>
2021-12-09 15:07:34 +0100

    benchmark v4

--- dynamic_allocator/benchmarks.cu
+++ dynamic_allocator/benchmarks.cu
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "dynamic_allocator.cu"
+#include "warp_malloc.cu"
 #include "benchmarks_separate.cu"
 
 
@@ -104,6 +105,28 @@ __global__ void sum_reduce_v1(int num_floats, clock_t* runtime_malloc, clock_t* 
     runtime_free[id] = end_free - start_free;
 }
 
+__global__ void sum_reduce_v4(int num_floats, clock_t* runtime_malloc, clock_t* runtime_work, clock_t* runtime_free) {
+    int id = (blockIdx.x*blockDim.x + threadIdx.x);
+    
+    clock_t start_malloc = clock64();
+    float* ptr = (float*)malloc_v4(num_floats * sizeof(float));
+    //printf("ptr_1, block %i: %p\n", blockIdx.x, ptr);
+    clock_t end_malloc = clock64();
+    runtime_malloc[id] = end_malloc - start_malloc;
+    
+	init_inc(num_floats, ptr);
+    
+    clock_t start_work = clock64();
+	sum_all_prod(num_floats, ptr);
+    clock_t end_work = clock64();
+    runtime_work[id] = end_work - start_work;
+    
+    clock_t start_free = clock64();
+    freev4(ptr);
+    clock_t end_free = clock64();
+    runtime_free[id] = end_free - start_free;
+}
+
 
 void print_arr(double* arr, int len) {
 	for (int i = 0; i < len; i++) {
@@ -153,6 +176,19 @@ int main(int argc, char **argv) {
             run_benchmark_separate(num_runs, num_warmup, mean_runtimes_malloc, mean_runtimes_work, mean_runtimes_free, max_runtimes_malloc, max_runtimes_work, max_runtimes_free, blocks, threads_per_block,
 				[num_floats](clock_t* runtimes_malloc, clock_t* runtimes_work, clock_t* runtimes_free, int b, int t) -> void {
 					sum_reduce_v1<<<b, t>>>(num_floats, runtimes_malloc, runtimes_work, runtimes_free);
+				}
+				 );
+			print_arr(mean_runtimes_malloc, num_runs);
+            print_arr(mean_runtimes_work, num_runs);
+            print_arr(mean_runtimes_free, num_runs);
+            //print_arr(max_runtimes_malloc, num_runs);
+            //print_arr(max_runtimes_work, num_runs);
+            //print_arr(max_runtimes_free, num_runs);
+            
+            // v4
+            run_benchmark_separate(num_runs, num_warmup, mean_runtimes_malloc, mean_runtimes_work, mean_runtimes_free, max_runtimes_malloc, max_runtimes_work, max_runtimes_free, blocks, threads_per_block,
+				[num_floats](clock_t* runtimes_malloc, clock_t* runtimes_work, clock_t* runtimes_free, int b, int t) -> void {
+					sum_reduce_v4<<<b, t>>>(num_floats, runtimes_malloc, runtimes_work, runtimes_free);
 				}
 				 );
 			print_arr(mean_runtimes_malloc, num_runs);
