6fd1646544162da202e90f7ab6a537b562a8726c
Martin Erhart <merhart@ethz.ch>
2021-12-09 08:20:37 +0100

    Align memory accesses to 128 bytes

--- gpu_libs/gpu_malloc/dyn_malloc.cu
+++ gpu_libs/gpu_malloc/dyn_malloc.cu
@@ -1,5 +1,6 @@
 #include "dyn_malloc.cuh"
 #include "cooperative_groups.h"
+#include <stdint.h>
 
 // Let's one thread allocate all memory for each block if
 // coalesced is set to true.
@@ -10,34 +11,36 @@
 // * Check synchronization/data races
 // * How to deal with mem? should it be __shared__ ?
 
-__shared__ static void **mem;
+__shared__ static void *mem;
 
 
 __device__ void *dyn_malloc(size_t size, bool coalesced) {
     if(!coalesced) {
         // Ensure compatibility with coalesced case
         // | counter 4B | ptr to counter 8B | returned ptr... |
-        void *ptr = malloc(size + sizeof(int*) + sizeof(int));
-        int *counter = (int*) ptr;
-        int **counter_ptr = (int**)(counter+1);
+        void *ptr = malloc(size +256);//+ sizeof(int*) + sizeof(int));
+        int32_t *counter = (int32_t*) ptr;
+        int32_t **counter_ptr = (int32_t**)(counter+32);
         *counter = 1;
         *counter_ptr = counter;
-        return counter_ptr+1;
+        return counter_ptr+16;
     }
 
+    size_t allocatePerThread = size % 128;
+    allocatePerThread = (128 - allocatePerThread) % 128 + 128 + size;
     if (threadIdx.x == 0) {
-        *mem = malloc((size + sizeof(int*)) * blockDim.x + sizeof(int));
+        mem = malloc(allocatePerThread * blockDim.x + 128);
         // Initialize counter
-        **(int**)mem = blockDim.x;
+        *(int32_t*)mem = blockDim.x;
     }
 
     auto block = cooperative_groups::this_thread_block();
 
     block.sync();
 
-    void *ptr = (char*)*mem + sizeof(int) + sizeof(int*) + threadIdx.x * (size + sizeof(int*));
-    int **counter_ptr = (int**) ((char*)ptr-sizeof(int*));
-    *counter_ptr = (int*)*mem;
+    void *ptr = (char*)mem + 128 + 128 + threadIdx.x * allocatePerThread;
+    int32_t **counter_ptr = (int32_t**) ((char*)ptr-128);
+    *counter_ptr = (int32_t*)mem;
 
     block.sync();
 
@@ -45,10 +48,10 @@ __device__ void *dyn_malloc(size_t size, bool coalesced) {
 }
 
 __device__ void dyn_free(void *memptr) {
-    int *counter_ptr = *((int**)memptr-1);
-    int counter = atomicAdd(counter_ptr, -1);
+    int32_t *counter_ptr = *(((int32_t**)memptr)-16);
+    int32_t counter = atomicAdd(counter_ptr, -1);
 
-    if (counter==0) {
+    if (counter==1) {
         free(counter_ptr);
     }
 }
