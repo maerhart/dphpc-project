4b009f3003db1778299bdd5ff859178d22708ba6
Martin Erhart <merhart@ethz.ch>
2021-12-22 19:40:25 +0100

    Adjust combined benchmark

--- mpi_malloc_benchmarks/combined.c
+++ mpi_malloc_benchmarks/combined.c
@@ -1,15 +1,23 @@
 #include "mpi.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <assert.h>
+
+double dotProduct(double *x, double *y, int n) {
+  int i;
+  double prod = 0.0;
+  for (i = 0; i < n; i++) {
+    prod += x[i]*y[i];
+  }
+  return prod;
+}
 
 int main(int argc, char *argv[]) {
   int i;
-  double prod_x, prod_y;
+  double prod;
   int my_rank;
   int num_procs;
   double start;
-  double t_malloc, t_work, t_free;
+  double t_malloc=0, t_work=0, t_free=0;
 
   if (argc != 2) return -1;
   int n_elems = atoi(argv[1]);
@@ -18,52 +26,63 @@ int main(int argc, char *argv[]) {
   MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
   MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
 
-  // measure malloc time
-  double *local_x;
-  double *local_y;
-  start = MPI_Wtime();
-  if (my_rank < num_procs / 2)
-    local_x = (double *) malloc(n_elems * sizeof(double));
-  if (my_rank == 0) local_y = (double *) malloc(n_elems * sizeof(double));
-  t_malloc = MPI_Wtime() - start;
+  if (my_rank < num_procs/2) {
+    start = MPI_Wtime();
+    double *local_x = (double *) malloc(n_elems * sizeof(double));
+    double *local_y = (double *) malloc(n_elems * sizeof(double));
+    t_malloc = MPI_Wtime() - start;
 
-
-  // init memory
-  if (my_rank < num_procs / 2) {
     for(i = 0; i < n_elems; i++) {
-      local_x[i] = i;
+      local_x[i] = 0.01 * i;
+      local_y[i] = 0.03 * i;
     }
+
+    double local_prod;
+    // Warmup
+    for (int j = 0; j < 3; ++j) {
+      local_prod = dotProduct(local_x,local_y,n_elems);
+    }
+    start = MPI_Wtime();
+    for (int j = 0; j < 500000; ++j) {
+      local_prod = dotProduct(local_x,local_y,n_elems);
+    }
+    t_work = MPI_Wtime() - start;
+    // MPI_Reduce(&local_prod, &prod, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+
+    start = MPI_Wtime();
+    free(local_x);
+    free(local_y);
+    t_free = MPI_Wtime() - start;
   }
 
+  start = MPI_Wtime();
+  double *local_x = (double *) malloc(n_elems * sizeof(double));
+  double *local_y = (double *) malloc(n_elems * sizeof(double));
+  t_malloc += MPI_Wtime() - start;
+
+  for(i = 0; i < n_elems; i++) {
+    local_x[i] = 0.01 * i;
+    local_y[i] = 0.03 * i;
+  }
+
+  double local_prod;
   // Warmup
-  if (my_rank < num_procs / 2)
-    MPI_Reduce(local_x, local_y, n_elems, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
-  // measure work to evaluate memory access time
-  start = MPI_Wtime();
-  if (my_rank < num_procs / 2)
-    MPI_Reduce(local_x, local_y, n_elems, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
-  t_work = MPI_Wtime() - start;
-
-  // check that the result of the computations are indeed correct
-  if (my_rank == 0) {
-    for (int i =0; i < n_elems; i++) {
-      assert(local_y[i] == i*num_procs/2);
-      if (local_y[i] != i*num_procs/2) {
-        printf("Expected %f, but got %f\n", local_y[i], (double)i*num_procs/2);
-        fflush(stdout);
-      }
-    }
+  for (int j = 0; j < 3; ++j) {
+    local_prod = dotProduct(local_x,local_y,n_elems);
   }
-
-  // measure free time
   start = MPI_Wtime();
-  if (my_rank < num_procs / 2) free(local_x);
-  if (my_rank == 0) free(local_y);
-  t_free = MPI_Wtime() - start;
+  for (int j = 0; j < 500000; ++j) {
+    local_prod = dotProduct(local_x,local_y,n_elems);
+  }
+  t_work += MPI_Wtime() - start;
+  // MPI_Reduce(&local_prod, &prod, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
 
+  start = MPI_Wtime();
+  free(local_x);
+  free(local_y);
+  t_free += MPI_Wtime() - start;
 
-  if (my_rank < num_procs / 2)
-    printf("%f %f %f\n", t_malloc, t_work, t_free);
+  printf("%f %f %f\n", t_malloc, t_work, t_free);
 
   MPI_Finalize();
 
